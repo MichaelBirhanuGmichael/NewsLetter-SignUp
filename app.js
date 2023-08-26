fd8c7db35df05895eec93d2bc912b406fb467163
Mickeybuye12 <136741155+Mickeybuye12@users.noreply.github.com>
2023-08-26 22:44:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post("/goback",function(req,res){
   res.redirect("/");
 })
 
-app.listen(process.env.PORT||3000, function () {
+app.listen(process.env.PORT|3000, function () {
   console.log("server is running on port 3000.");
 });
 
